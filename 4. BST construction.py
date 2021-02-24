ae654a46c8cdbeb069481d7f7aaad177c2de4498
manojadhav <manojashokajadhav@gmail.com>
2021-02-24 21:59:40 +0530

    Add files via upload

--- 4. BST construction.py
+++ 4. BST construction.py
@@ -1,28 +1,5 @@
 class BST:
     def __init__(self,value):
-        self.value = value
-        self.right = None
-        self.left = None
-    def insert(self,value):
-        currentnode = self
-        While True:
-            if value < currentnode.value:
-                if currentnode.left = None:
-                    currentnode.left = BST(value)
-                    break
-                else:
-                currentnode = currentnode.left
-            else:
-                if currentnode.right = None:
-                    currentnode.right = BST(value)
-                    break
-                else:
-                currentnode = currentnode.right
-        return self
-        
-
-class BST:
-    def __init__(self,value):
         self.value = value:
         self.right = None
         self.left = None
